7eaa3369153bbf6b4a957983f5eb7928befec825
Dev Karan Singh <dev1604karan@gmail.com>
2025-07-17 22:22:46 +0530

    day 4: update app icon paths and enhance tab navigation with dynamic icons for Home, Search, Saved, and Profile

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,40 +5,67 @@ import React from "react";
 import {
   Image,
   ImageBackground,
+  SafeAreaView as SafeArea,
   Text,
+  View,
 } from "react-native";
 
-const TabIcon = () => {
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: any) => {
+  if (focused) {
+    return (
+      <SafeArea>
+        <ImageBackground
+          source={images.highlight}
+          className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        >
+          <Image
+            source={icon}
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: "#151312",
+            }}
+          />
+          <Text className="text-secondary text-base font-semibold ml-2">
+            {title}
+          </Text>
+        </ImageBackground>
+      </SafeArea>
+    );
+  }
   return (
-    <ImageBackground
-      source={images.highlight}
-      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-    >
-      <Image
-        source={icons.home}
-        style={{
-          width: 20,
-          height: 20,
-          tintColor: "#151312",
-        }}
-      />
-      <Text className="text-secondary text-base font-semibold ml-2">
-        Home
-      </Text>
-    </ImageBackground>
+    <SafeArea>
+      <View className="size-full justify-center items-center mt-4 rounded-full">
+        <Image
+          source={icon}
+          tintColor={"#A8B5DB"}
+          className="size-5"
+        ></Image>
+      </View>
+    </SafeArea>
   );
 };
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{ tabBarShowLabel: false }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <TabIcon />
+            <TabIcon
+              focused={focused}
+              icon={icons.home}
+              title={"Home"}
+            />
           ),
         }}
       />
@@ -47,6 +74,13 @@ const _layout = () => {
         options={{
           title: "Search",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.search}
+              title={"Search"}
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -54,6 +88,13 @@ const _layout = () => {
         options={{
           title: "Saved",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.save}
+              title={"Saved"}
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -61,6 +102,13 @@ const _layout = () => {
         options={{
           title: "Profile",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.person}
+              title={"Profile"}
+            />
+          ),
         }}
       />
     </Tabs>
